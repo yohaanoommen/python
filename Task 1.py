9a9b38620e97e47df136953b45c3a1ed8cecb998
yohaanoommen <yohaanoommen@gmail.com>
2025-06-07 12:26:49 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,12 @@
+f=input('Enter the first number: ')
+s=input('Enter the Second number: ')
+f=int(f)
+s=int(s)
+a=f + s
+b=f - s
+c=f * s
+d=f/s
+print('addition: ',a)
+print('subtraction: ',b)
+print('multiplication: ',c)
+print('division: ',d)
